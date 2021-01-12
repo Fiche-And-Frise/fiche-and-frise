006b7115bb8935de82a081227c8d97f5164183ab
Yanis Hamidouche <yanis.hamidouche.etu@univ-lille.fr>
2021-01-12 16:57:23 +0100

    Modification de l'ecran d'accueil et correction bug changement d'activité

--- app/src/main/java/com/example/fichefrise/presentation/display/MainActivity.java
+++ app/src/main/java/com/example/fichefrise/presentation/display/MainActivity.java
@@ -2,15 +2,33 @@ package com.example.fichefrise.presentation.display;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 
 import com.example.fichefrise.R;
+import com.example.fichefrise.presentation.display.fiche.DetailFicheActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton ficheButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ficheButton = findViewById(R.id.fiche_button);
+        ficheButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, DetailFicheActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
+
 }
