be1d32804d87bab64ec2c35e3b766e46bfe0128a
Yanis Hamidouche <yanis.hamidouche.etu@univ-lille.fr>
2021-01-16 23:23:57 +0100

    Ajout de l'entité Fiche et ajout de dépendances au projet

---
+++ app/src/main/java/com/example/fichefrise/data/api/model/Fiche.java
@@ -0,0 +1,61 @@
+package com.example.fichefrise.data.api.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Fiche {
+
+    @SerializedName("id")
+    private int ficheId;
+
+    @SerializedName("name")
+    private String nomFiche;
+
+    @SerializedName("theme")
+    private String themeFiche;
+
+    @SerializedName("recto")
+    private String recto;
+
+    @SerializedName("verso")
+    private String verso;
+
+    public int getFicheId() {
+        return ficheId;
+    }
+
+    public void setFicheId(int ficheId) {
+        this.ficheId = ficheId;
+    }
+
+    public String getNomFiche() {
+        return nomFiche;
+    }
+
+    public void setNomFiche(String nomFiche) {
+        this.nomFiche = nomFiche;
+    }
+
+    public String getThemeFiche() {
+        return themeFiche;
+    }
+
+    public void setThemeFiche(String themeFiche) {
+        this.themeFiche = themeFiche;
+    }
+
+    public String getRecto() {
+        return recto;
+    }
+
+    public void setRecto(String recto) {
+        this.recto = recto;
+    }
+
+    public String getVerso() {
+        return verso;
+    }
+
+    public void setVerso(String verso) {
+        this.verso = verso;
+    }
+}
