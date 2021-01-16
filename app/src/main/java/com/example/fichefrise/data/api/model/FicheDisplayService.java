8676fe44eedd3357f2aae8b8145a57479b109b86
Yanis Hamidouche <yanis.hamidouche.etu@univ-lille.fr>
2021-01-16 23:28:13 +0100

    Ajout de l'interface FicheDisplayService

---
+++ app/src/main/java/com/example/fichefrise/data/api/model/FicheDisplayService.java
@@ -0,0 +1,17 @@
+package com.example.fichefrise.data.api.model;
+
+import java.util.List;
+
+import io.reactivex.Single;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface FicheDisplayService {
+
+    @GET("fiches")
+    Single<List<Fiche>> getAllFiches();
+
+    @GET("fiches/{id}")
+    Single<Fiche> getFicheById(@Path("id") int ficheId);
+
+}
