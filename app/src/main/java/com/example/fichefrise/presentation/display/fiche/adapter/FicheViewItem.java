681ae73a8ebfd7d7b2d7db9bd6c38cd5eef2a198
Yanis Hamidouche <yanis.hamidouche.etu@univ-lille.fr>
2021-01-17 22:09:45 +0100

    Ajout de FicheViewItem

---
+++ app/src/main/java/com/example/fichefrise/presentation/display/fiche/adapter/FicheViewItem.java
@@ -0,0 +1,22 @@
+package com.example.fichefrise.presentation.display.fiche.adapter;
+
+public class FicheViewItem {
+    private int ficheId;
+    private String nomFiche;
+
+    public int getFicheId() {
+        return ficheId;
+    }
+
+    public void setFicheId(int ficheId) {
+        this.ficheId = ficheId;
+    }
+
+    public String getNomFiche() {
+        return nomFiche;
+    }
+
+    public void setNomFiche(String nomFiche) {
+        this.nomFiche = nomFiche;
+    }
+}
