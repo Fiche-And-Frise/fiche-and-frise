4bd718726a764f18804f00651be854758502c30a
unknown <maxime.hilberer.etu@univ-lille.fr>
2021-01-18 15:46:03 +0100

    Ajout de l'adapter pour la liste de fiche

--- app/src/main/java/com/example/fichefrise/presentation/display/fiche/FichesListActivity.java
+++ app/src/main/java/com/example/fichefrise/presentation/display/fiche/FichesListActivity.java
@@ -2,6 +2,9 @@ package com.example.fichefrise.presentation.display.fiche;
 
 import android.os.Bundle;
 
+import com.example.fichefrise.data.api.model.Fiche;
+import com.example.fichefrise.presentation.display.fiche.adapter.FicheAdapter;
+import com.example.fichefrise.presentation.display.fiche.adapter.FicheViewItem;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -10,14 +13,20 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.View;
 
 import com.example.fichefrise.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FichesListActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private RecyclerView.LayoutManager layoutManager;
+    private FicheAdapter adapter;
+    private List<FicheViewItem> fichesListStatic = new ArrayList<>();
 
     private Toolbar toolbar;
 
@@ -29,6 +38,8 @@ public class FichesListActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Liste des fiches");
+
+        createFiches();
 
         setupRecyclerView();
         /*FloatingActionButton fab = findViewById(R.id.fab);
@@ -44,5 +55,30 @@ public class FichesListActivity extends AppCompatActivity {
     private void setupRecyclerView() {
         recyclerView =findViewById(R.id.fiches_recyclerview);
         layoutManager = new LinearLayoutManager(this);
+        adapter = new FicheAdapter();
+        adapter.bindFicheViewModelList(fichesListStatic);
+        recyclerView.setAdapter(this.adapter);
+        recyclerView.setLayoutManager(this.layoutManager);
+    }
+
+    private void createFiches(){
+        ArrayList<FicheViewItem> list = new ArrayList<>();
+        FicheViewItem f1 = new FicheViewItem();
+        f1.setFicheId(1);
+        f1.setNomFiche("La première fiche");
+        list.add(f1);
+
+        FicheViewItem f2 = new FicheViewItem();
+        f2.setFicheId(2);
+        f2.setNomFiche("La deuxième fiche");
+        list.add(f2);
+
+        FicheViewItem f3 = new FicheViewItem();
+        f3.setFicheId(3);
+        f3.setNomFiche("La première guerre mondiale");
+        list.add(f3);
+
+        this.fichesListStatic = list;
+
     }
 }
---
+++ app/src/main/java/com/example/fichefrise/presentation/display/fiche/adapter/FicheAdapter.java
@@ -0,0 +1,60 @@
+package com.example.fichefrise.presentation.display.fiche.adapter;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.fichefrise.R;
+
+import java.util.List;
+
+public class FicheAdapter extends RecyclerView.Adapter<FicheAdapter.FicheViewHolder> {
+
+    private List<FicheViewItem> viewItemList;
+
+    @NonNull
+    @Override
+    public FicheViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_fiche_recyclerview, parent, false);
+        return new FicheViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FicheViewHolder holder, int position) {
+        holder.bind(viewItemList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.viewItemList.size();
+    }
+
+    public void bindFicheViewModelList(List<FicheViewItem> fichesListStatic) {
+        this.viewItemList = fichesListStatic;
+        notifyDataSetChanged();
+    }
+
+    public static class FicheViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView ficheNameTextView;
+        private View v;
+        private FicheViewItem ficheViewItem;
+
+        public FicheViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.v = itemView;
+            this.ficheNameTextView =itemView.findViewById(R.id.ficheNameTextView);
+        }
+
+        public void bind(FicheViewItem item){
+            this.ficheViewItem = item;
+            this.ficheNameTextView.setText(ficheViewItem.getNomFiche());
+        }
+    }
+}
