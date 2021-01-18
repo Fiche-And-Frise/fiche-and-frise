f4d6251c993cc5512395aeee6309562dfa430ee1
unknown <maxime.hilberer.etu@univ-lille.fr>
2021-01-18 16:15:18 +0100

    Ajout de l'entité Thème

---
+++ app/src/main/java/com/example/fichefrise/data/api/model/Theme.java
@@ -0,0 +1,43 @@
+package com.example.fichefrise.data.api.model;
+
+import java.util.List;
+
+public class Theme {
+
+    private String name;
+    private String color;
+    private List<Fiche> fiches;
+    private List<Frise> frises;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public List<Fiche> getFiches() {
+        return fiches;
+    }
+
+    public void setFiches(List<Fiche> fiches) {
+        this.fiches = fiches;
+    }
+
+    public List<Frise> getFrises() {
+        return frises;
+    }
+
+    public void setFrises(List<Frise> frises) {
+        this.frises = frises;
+    }
+}
